1c5053a7dbf3490b82267960594cfa8be61f8bd7
VenomiXeD <crafterious@gmail.com>
2025-11-27 02:10:14 +0100

    Move folder

---
+++ VideoExporter/VideoExporter/VideoExporterContextMenu.cs
@@ -0,0 +1,80 @@
+﻿using Frosty.Core;
+using Frosty.Core.Attributes;
+using FrostySdk.Interfaces;
+using System.Diagnostics;
+using System.Reflection;
+using VideoViewer;
+
+// [assembly: RegisterStartupAction(typeof(BestPlugin))]
+[assembly:PluginAuthor("MeanPartyRose, SprettWasHere")]
+[assembly:PluginDisplayName("Video Exporter")]
+[assembly:PluginVersion("1.0.0.0")]
+[assembly:RegisterDataExplorerContextMenu(typeof(VideoExporterContextMenu))]
+
+namespace VideoViewer
+{
+    public class VideoExporterContextMenu : DataExplorerContextMenuExtension
+    {
+        public override string ContextItemName => "Export Video";
+        public override RelayCommand ContextItemClicked => new RelayCommand(OnContextMenuClicked);
+
+        private async void OnContextMenuClicked(object obj)
+        {
+            object? ebxObject = App.AssetManager.GetEbx(App.SelectedAsset)?.RootObject;
+            PropertyInfo chunkGuidProperty = ebxObject?.GetType().GetProperty("ChunkGuid");
+            if (chunkGuidProperty is null)
+            {
+                App.Logger.LogWarning("Selected Asset has no ChunkGuid to refer to.");
+                return;
+            }
+
+            Guid chunkGuid = Guid.Parse(chunkGuidProperty.GetValue(ebxObject).ToString());
+
+
+
+            Directory.CreateDirectory("exported");
+            using FileStream fs = File.Create($"./exported/{chunkGuid.ToString()}.vp6");
+            using Stream chunkStream = App.AssetManager.GetChunk(App.AssetManager.GetChunkEntry(chunkGuid));
+            chunkStream.CopyTo(fs);
+
+            fs.Close();
+            fs.Dispose();
+
+            string inputFile = Path.Combine(AppContext.BaseDirectory, "exported", $"{chunkGuid.ToString()}.vp6");
+            string outputFileBaseMp4 = Path.Combine(AppContext.BaseDirectory, "exported", $"{App.SelectedAsset.DisplayName}-vid.mp4");
+            string outputFileFullMp4 = Path.Combine(AppContext.BaseDirectory, "exported", $"{App.SelectedAsset.DisplayName}-merged.mp4");
+            string outputFileAudioWav = Path.Combine(AppContext.BaseDirectory, "exported", $"{App.SelectedAsset.DisplayName}.wav");
+
+            // Convert it to a mp4 first
+            ProcessStartInfo processStart;
+            processStart = new ProcessStartInfo(VideoExporterPlugin.FFMPEG_EXECUTABLE)
+                { UseShellExecute = false };
+
+            processStart.Arguments = $"-i \"{inputFile}\" -c:v libx264 -crf 18 -preset slow -c:a aac -b:a 192k \"{outputFileBaseMp4}\"";
+            App.Logger.Log($"[EXEC] {processStart.FileName} {processStart.Arguments}");
+            await Task.Delay(100);
+            Process.Start(processStart).WaitForExit();
+
+            
+            // Get the wav output of the chunk
+            processStart = new ProcessStartInfo(VideoExporterPlugin.VGMSTREAM_EXECUTABLE)
+                { UseShellExecute = false };
+            processStart.Arguments = $"-o \"{outputFileAudioWav}\" \"{inputFile}\"";
+            
+            App.Logger.Log($"[EXEC] {processStart.FileName} {processStart.Arguments}");
+            await Task.Delay(100);
+            Process.Start(processStart).WaitForExit();
+            
+            // Merge wav and video
+            // ffmpeg -i input_video.mp4 -i input_audio.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 output.mp4
+            processStart = new ProcessStartInfo(VideoExporterPlugin.FFMPEG_EXECUTABLE)
+                { UseShellExecute = false };
+            processStart.Arguments =
+                $"-i \"{outputFileBaseMp4}\" -i \"{outputFileAudioWav}\" -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 \"{outputFileFullMp4}\"";
+            
+            App.Logger.Log($"[EXEC] {processStart.FileName} {processStart.Arguments}");
+            await Task.Delay(100);
+            Process.Start(processStart).WaitForExit();
+        }
+    }
+}
---
+++ VideoExporter/VideoExporter/VideoExporterPlugin.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+using Frosty.Core;
+using Frosty.Core.Attributes;
+using FrostySdk.Interfaces;
+using SharpCompress.Archives;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Readers;
+using VideoViewer;
+
+[assembly: RegisterStartupAction(typeof(VideoExporterPlugin))]
+
+namespace VideoViewer;
+
+public class VideoExporterPlugin : StartupAction
+{
+    private const string VGMSTREAM_DOWNLOAD_URL =
+        "https://github.com/vgmstream/vgmstream-releases/releases/download/nightly/vgmstream-win64.zip";
+
+    private const string FFMPEG_DOWNLOAD_URL = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z";
+    
+    private const string SEVENZIP_CLI_URL = "https://www.7-zip.org/a/7zr.exe";
+    private static string UTILS_FOLDER => Path.GetFullPath("./videoexporter_utils");
+
+    private static string FFMPEG_FOLDER => Path.Combine(UTILS_FOLDER, "ffmpeg");
+    private static string VGMSTREAM_FOLDER => Path.Combine(UTILS_FOLDER, "vgmstream");
+
+    public static string FFMPEG_EXECUTABLE => !Directory.Exists(FFMPEG_FOLDER) ? null : Path.Combine(Directory.GetDirectories(FFMPEG_FOLDER).FirstOrDefault(), "bin", "ffmpeg.exe");
+
+    public static string VGMSTREAM_EXECUTABLE => !Directory.Exists(VGMSTREAM_FOLDER) ? null : Path.Combine(VGMSTREAM_FOLDER, "vgmstream-cli.exe");
+
+    public override Action<ILogger> Action => OnStartup;
+
+    private async void OnStartup(ILogger obj)
+    {
+        Directory.CreateDirectory(UTILS_FOLDER);
+
+        using (WebClient wc = new WebClient())
+        {
+            wc.DownloadProgressChanged += (sender, args) => App.Logger.Log($"Downloading progress: {args.ProgressPercentage}%");
+            
+            if (!Directory.Exists(Path.Combine(UTILS_FOLDER, "ffmpeg")))
+            {
+                DialogResult dialogResult = MessageBox.Show("FFmpeg is not installed\nDo you wish to set it up?",
+                    "Missing ffmpeg",
+                    MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    string ffmpeg7z = Path.Combine(UTILS_FOLDER, "ffmpeg.7z");
+
+                    await wc.DownloadFileTaskAsync(FFMPEG_DOWNLOAD_URL, ffmpeg7z);
+                    await wc.DownloadFileTaskAsync(SEVENZIP_CLI_URL, Path.Combine(UTILS_FOLDER, "7zr.exe"));
+
+                    ProcessStartInfo info = new ProcessStartInfo(Path.Combine(UTILS_FOLDER, "7zr.exe"));
+                    info.UseShellExecute = false;
+                    info.Arguments = $"x \"{ffmpeg7z}\" -o\"{FFMPEG_FOLDER}\" -y";
+                    
+                    Process.Start(info).WaitForExit();
+                }
+            }
+
+            if (!Directory.Exists(VGMSTREAM_FOLDER))
+            {
+                DialogResult dialogResult = MessageBox.Show("VGMStream is not installed\nDo you wish to set it up?",
+                    "Missing VGMStream",
+                    MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    string vgmstreamZip = Path.Combine(UTILS_FOLDER, "vgmstream.zip");
+                    await wc.DownloadFileTaskAsync(VGMSTREAM_DOWNLOAD_URL, vgmstreamZip);
+                    
+                    ZipFile.ExtractToDirectory(vgmstreamZip, VGMSTREAM_FOLDER);
+                }
+            }
+        }
+    }
+}
